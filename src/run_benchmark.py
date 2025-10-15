ea40a4de8844ba0d25495ac65812faa8143992e5
bastis007 <schaefer.bastian@mail.de>
2025-10-15 16:17:33 +0200

    Fix Benchmark issues with the method name, upload first results

--- src/run_benchmark.py
+++ src/run_benchmark.py
@@ -10,7 +10,6 @@ from src.utils.run_models import get_model_score_origin_classification, get_mode
 
 
 def main():
-    print(fold)
     target_label = 'target'
 
     result_files = glob.glob("data/*/*.parquet")
@@ -18,12 +17,7 @@ def main():
 
     dataset_files = defaultdict(list)
     for f in result_files:
-        if f.__contains__("filter"):
-            dataset_id = f.split('.')[0].split('/')[-1].split('_')[-1]
-        elif f.__contains__("metalearning"):
-            dataset_id = f.split('.')[0].split('/')[-1].split('_')[-1]
-        else:
-            dataset_id = f.split('.')[0].split('/')[-1]
+        dataset_id = f.split('.')[0].split('/')[-1].split('_')[-1]
         dataset_files[dataset_id].append(f)
 
     for dataset_id, files in dataset_files.items():
@@ -34,7 +28,7 @@ def main():
         data = concat_data(X_train, y_train, X_test, y_test, "target")
         data.to_parquet(f"data/original/Original_{dataset_id}.parquet")
         # === ORIGINAL RESULTS ===
-        original_path = f"data/original/Original_{dataset_id}.parquet"
+        original_path = f"results/Original_{dataset_id}.parquet"
         try:
             original_results = pd.read_parquet(original_path)
         except FileNotFoundError:
@@ -59,31 +53,31 @@ def main():
             else:
                 print(f"  Processing file: {data_file}")
                 name = data_file.split('.')[0]
-
-                method = name.split('/')[-1]
-                result_path = f"results/{method}.parquet"
+                method_and_dataset = name.split('/')[-1]
+                method_name = method_and_dataset.split('_')[0]
+                result_path = f"results/{method_and_dataset}.parquet"
                 try:
                     results = pd.read_parquet(result_path)
                 except (FileNotFoundError, pyarrow.lib.ArrowInvalid):
                     try:
                         df = pd.read_parquet(data_file)
                         Xf_train, yf_train, Xf_test, yf_test = split_data(df, target_label)
-                        if task_type == "Supervised Classification":
-                            results = get_model_score_origin_classification(Xf_train, yf_train, Xf_test, yf_test, dataset_id, method)
+                        if task_type == 'Supervised Classification':
+                            results = get_model_score_origin_classification(Xf_train, yf_train, Xf_test, yf_test, dataset_id, method_name)
                         else:
-                            results = get_model_score_origin_regression(Xf_train, yf_train, Xf_test, yf_test, dataset_id, method)
-                        results = results[results['model'] == "LightGBM_BAG_L1"]
+                            results = get_model_score_origin_regression(Xf_train, yf_train, Xf_test, yf_test, dataset_id, method_name)
+                        results = results[results['model'] == 'LightGBM_BAG_L1']
                         results.to_parquet(result_path)
                     except KeyError:
-                        print("No data file")
+                        print('No data file')
                         continue
                     combined_results.append(results)
                 else:
                     combined_results.append(results)
 
         all_results = pd.concat(combined_results, ignore_index=True).drop_duplicates()
-        all_results.to_parquet(f"results/Result_{dataset_id}.parquet")
-        print(f"Saved combined results for dataset {dataset_id} and fold {fold}.")
+        all_results.to_parquet(f'results/Result_{dataset_id}.parquet')
+        print(f'Saved combined results for dataset {dataset_id}.')
 
 
 if __name__ == "__main__":
