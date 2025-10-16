1d69a76d306a3883317cfa1eb9660bbd871bd85b
bastis007 <schaefer.bastian@mail.de>
2025-10-16 15:29:19 +0200

    Include Featurewiz

--- src/feature_selection_method/wrapper/Featurewiz/Featurewiz.py
+++ src/feature_selection_method/wrapper/Featurewiz/Featurewiz.py
@@ -1,7 +1,9 @@
 # https://github.com/AutoViML/featurewiz
 
 import pandas as pd
-from src.feature_engineering.Featurewiz.method.featurewiz import FeatureWiz
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FeatureWiz
+
+from src.utils.get_data import get_openml_dataset_split_and_metadata, concat_data
 
 
 def get_featurewiz_features(train_x, train_y, test_x) -> tuple[
@@ -14,3 +16,18 @@ def get_featurewiz_features(train_x, train_y, test_x) -> tuple[
     train_x, train_y = fwiz.fit_transform(train_x, train_y)
     test_x = fwiz.transform(test_x)
     return train_x, test_x
+
+
+def main():
+    dataset_id = 146820
+    try:
+        pd.read_parquet("../../../data/wrapper/Featurewiz_" + str(dataset_id) + ".parquet")
+    except FileNotFoundError:
+        X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(dataset_id)
+        X_train, X_test = get_featurewiz_features(X_train, y_train, X_test)
+        data = concat_data(X_train, y_train, X_test, y_test, "target")
+        data.to_parquet("../../../data/wrapper/Featurewiz_" + str(dataset_id) + ".parquet")
+
+
+if __name__ == "__main__":
+    main()
--- src/feature_selection_method/wrapper/Featurewiz/method/__init__.py
+++ src/feature_selection_method/wrapper/Featurewiz/method/__init__.py
@@ -6,44 +6,44 @@
 #     Licensed under Apache License v2
 ################################################################################
 # Version
-from .__version__ import __version__
-from .featurewiz import featurewiz
-from .featurewiz import FE_split_one_field_into_many, FE_add_groupby_features_aggregated_to_dataframe
-from .featurewiz import FE_start_end_date_time_features
-from .featurewiz import classify_features
-from .featurewiz import classify_columns,FE_combine_rare_categories
-from .featurewiz import FE_count_rows_for_all_columns_by_group
-from .featurewiz import FE_add_age_by_date_col, FE_split_add_column, FE_get_latest_values_based_on_date_column
-from .featurewiz import FE_capping_outliers_beyond_IQR_Range
-from .featurewiz import EDA_classify_and_return_cols_by_type, EDA_classify_features_for_deep_learning
-from .featurewiz import FE_create_categorical_feature_crosses, EDA_find_skewed_variables
-from .featurewiz import FE_kmeans_resampler, FE_find_and_cap_outliers, EDA_find_outliers
-from .featurewiz import split_data_n_ways, FE_concatenate_multiple_columns
-from .featurewiz import FE_discretize_numeric_variables, reduce_mem_usage
-from .ml_models import simple_XGBoost_model, simple_LightGBM_model, complex_XGBoost_model
-from .ml_models import complex_LightGBM_model,data_transform, MultiClassSVM
-from .ml_models import IterativeBestClassifier, IterativeDoubleClassifier, IterativeSearchClassifier
-from .my_encoders import My_LabelEncoder, Groupby_Aggregator, My_LabelEncoder_Pipe, Ranking_Aggregator, DateTime_Transformer
-from .my_encoders import Rare_Class_Combiner, Rare_Class_Combiner_Pipe, FE_create_time_series_features, Binning_Transformer
-from .my_encoders import Column_Names_Transformer, FE_convert_all_object_columns_to_numeric, Numeric_Transformer
-from .my_encoders import TS_Lagging_Transformer, TS_Fourier_Transformer, TS_Trend_Seasonality_Transformer
-from .my_encoders import TS_Lagging_Transformer_Pipe, TS_Fourier_Transformer_Pipe
+from src.feature_selection_method.wrapper.Featurewiz.method.__version__ import __version__
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import featurewiz
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_split_one_field_into_many, FE_add_groupby_features_aggregated_to_dataframe
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_start_end_date_time_features
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import classify_features
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import classify_columns,FE_combine_rare_categories
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_count_rows_for_all_columns_by_group
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_add_age_by_date_col, FE_split_add_column, FE_get_latest_values_based_on_date_column
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_capping_outliers_beyond_IQR_Range
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import EDA_classify_and_return_cols_by_type, EDA_classify_features_for_deep_learning
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_create_categorical_feature_crosses, EDA_find_skewed_variables
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_kmeans_resampler, FE_find_and_cap_outliers, EDA_find_outliers
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import split_data_n_ways, FE_concatenate_multiple_columns
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_discretize_numeric_variables, reduce_mem_usage
+from src.feature_selection_method.wrapper.Featurewiz.method.ml_models import simple_XGBoost_model, simple_LightGBM_model, complex_XGBoost_model
+from src.feature_selection_method.wrapper.Featurewiz.method.ml_models import complex_LightGBM_model,data_transform, MultiClassSVM
+from src.feature_selection_method.wrapper.Featurewiz.method.ml_models import IterativeBestClassifier, IterativeDoubleClassifier, IterativeSearchClassifier
+from src.feature_selection_method.wrapper.Featurewiz.method.my_encoders import My_LabelEncoder, Groupby_Aggregator, My_LabelEncoder_Pipe, Ranking_Aggregator, DateTime_Transformer
+from src.feature_selection_method.wrapper.Featurewiz.method.my_encoders import Rare_Class_Combiner, Rare_Class_Combiner_Pipe, FE_create_time_series_features, Binning_Transformer
+from src.feature_selection_method.wrapper.Featurewiz.method.my_encoders import Column_Names_Transformer, FE_convert_all_object_columns_to_numeric, Numeric_Transformer
+from src.feature_selection_method.wrapper.Featurewiz.method.my_encoders import TS_Lagging_Transformer, TS_Fourier_Transformer, TS_Trend_Seasonality_Transformer
+from src.feature_selection_method.wrapper.Featurewiz.method.my_encoders import TS_Lagging_Transformer_Pipe, TS_Fourier_Transformer_Pipe
 from lazytransform import LazyTransformer, SuloRegressor, SuloClassifier, print_regression_metrics, print_classification_metrics
 from lazytransform import print_regression_model_stats, YTransformer, print_sulo_accuracy
-from .sulov_method import FE_remove_variables_using_SULOV_method
-from .featurewiz import FE_transform_numeric_columns_to_bins, FE_create_interaction_vars
-from .stacking_models import Stacking_Classifier, Blending_Regressor, Stacking_Regressor, stacking_models_list
-from .stacking_models import StackingClassifier_Multi, analyze_problem_type_array, get_class_distribution
-from .auto_encoders import DenoisingAutoEncoder, VariationalAutoEncoder, CNNAutoEncoder
-from .auto_encoders import GAN, GANAugmenter
-from .featurewiz import EDA_binning_numeric_column_displaying_bins, FE_calculate_duration_from_timestamp
-from .featurewiz import FE_convert_mixed_datatypes_to_string, FE_drop_rows_with_infinity
-from .featurewiz import EDA_find_remove_columns_with_infinity, FE_split_list_into_columns
-from .featurewiz import EDA_remove_special_chars, FE_remove_commas_in_numerics
-from .featurewiz import EDA_randomly_select_rows_from_dataframe, remove_duplicate_cols_in_dataset
-from .featurewiz import cross_val_model_predictions
-from .blagging import BlaggingClassifier
-from .featurewiz import FeatureWiz
+from src.feature_selection_method.wrapper.Featurewiz.method.sulov_method import FE_remove_variables_using_SULOV_method
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_transform_numeric_columns_to_bins, FE_create_interaction_vars
+from src.feature_selection_method.wrapper.Featurewiz.method.stacking_models import Stacking_Classifier, Blending_Regressor, Stacking_Regressor, stacking_models_list
+from src.feature_selection_method.wrapper.Featurewiz.method.stacking_models import StackingClassifier_Multi, analyze_problem_type_array, get_class_distribution
+from src.feature_selection_method.wrapper.Featurewiz.method.auto_encoders import DenoisingAutoEncoder, VariationalAutoEncoder, CNNAutoEncoder
+from src.feature_selection_method.wrapper.Featurewiz.method.auto_encoders import GAN, GANAugmenter
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import EDA_binning_numeric_column_displaying_bins, FE_calculate_duration_from_timestamp
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FE_convert_mixed_datatypes_to_string, FE_drop_rows_with_infinity
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import EDA_find_remove_columns_with_infinity, FE_split_list_into_columns
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import EDA_remove_special_chars, FE_remove_commas_in_numerics
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import EDA_randomly_select_rows_from_dataframe, remove_duplicate_cols_in_dataset
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import cross_val_model_predictions
+from src.feature_selection_method.wrapper.Featurewiz.method.blagging import BlaggingClassifier
+from src.feature_selection_method.wrapper.Featurewiz.method.featurewiz import FeatureWiz
 ################################################################################
 if __name__ == "__main__":
     module_type = 'Running'
