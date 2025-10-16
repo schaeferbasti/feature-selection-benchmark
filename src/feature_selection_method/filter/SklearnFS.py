51239f5397b6ef82045d7ec1d02ec2066cc28dd8
bastis007 <schaefer.bastian@mail.de>
2025-10-16 10:44:12 +0200

    Add Sklearn Select K Best with different Score Functions, Select Percentile

--- src/feature_selection_method/filter/SklearnFS.py
+++ src/feature_selection_method/filter/SklearnFS.py
@@ -8,7 +8,8 @@ import ctypes
 
 import pandas as pd
 
-from sklearn.feature_selection import SelectKBest, f_classif, SelectFromModel, VarianceThreshold
+from sklearn.feature_selection import SelectKBest, f_classif, SelectFromModel, VarianceThreshold, SelectPercentile, \
+    mutual_info_classif, chi2
 from sklearn.ensemble import ExtraTreesClassifier
 from sklearn.svm import LinearSVC
 
@@ -20,57 +21,110 @@ def process_method(dataset_id):
     last_reset_time.value = time.time()
     X_train, y_train, X_test, y_test, dataset_metadata = get_openml_dataset_split_and_metadata(dataset_id)
 
+    # Variance Threshold
+    print("Filter Method: Variance Threshold, Dataset: " + str(dataset_id))
+    try:
+        data = pd.read_parquet("../../data/filter/SklearnVarianceThreshold_Ad" + str(dataset_id) + ".parquet")
+        print("File exists, next method" + str(data.head()) + "\n\n")
+    except FileNotFoundError:
+        print("Calculate Feature Selection \n\n")
+        variance_threshold = VarianceThreshold(threshold=(.8 * (1 - .8)))
+        X_train_new = variance_threshold.fit_transform(X_train)
+        X_test_new = variance_threshold.transform(X_test)
+        selected_features = X_train.columns[variance_threshold.get_support()]
+        X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
+        X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
+        data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
+        data.to_parquet("../../data/filter/SklearnVarianceThreshold_" + str(dataset_id) + ".parquet")
 
-    # Variance Treshold
-    variance_threshold = VarianceThreshold(threshold=(.8 * (1 - .8)))
-    X_train_new = variance_threshold.fit_transform(X_train)
-    X_test_new = variance_threshold.transform(X_test)
-    selected_features = X_train.columns[variance_threshold.get_support()]
-    X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
-    X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
-    data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
-    data.to_parquet("../../data/filter/SklearnVarianceThreshold_" + str(dataset_id) + ".parquet")
+    # SelectKBest - Classif_f
+    print("Filter Method: SelectKBest, Score Function: Classif F, Dataset: " + str(dataset_id))
+    try:
+        data = pd.read_parquet("../../data/filter/SelectKBestFClassif_" + str(dataset_id) + ".parquet")
+        print("File exists, next method" + str(data.head()) + "\n\n")
+    except FileNotFoundError:
+        print("Calculate Feature Selection \n\n")
+        selectKBest = SelectKBest(score_func=f_classif, k=3)
+        selectKBest.fit(X_train, y_train)
+        X_train_new = selectKBest.transform(X_train)
+        X_test_new = selectKBest.transform(X_test)
+        selected_features = X_train.columns[selectKBest.get_support()]
+        # Transform the data and wrap it back into DataFrames
+        X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
+        X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
+        data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
+        data.to_parquet("../../data/filter/SelectKBestFClassif_" + str(dataset_id) + ".parquet")
 
+    # SelectKBest - Chi2
+    print("Filter Method: SelectKBest, Score Function: Chi2, Dataset: " + str(dataset_id))
+    try:
+        data = pd.read_parquet("../../data/filter/SelectKBestChi2_" + str(dataset_id) + ".parquet")
+        print("File exists, next method" + str(data.head()) + "\n\n")
+    except FileNotFoundError:
+        print("Calculate Feature Selection \n\n")
+        selectKBest = SelectKBest(score_func=chi2, k=3)
+        selectKBest.fit(X_train, y_train)
+        X_train_new = selectKBest.transform(X_train)
+        X_test_new = selectKBest.transform(X_test)
+        selected_features = X_train.columns[selectKBest.get_support()]
+        # Transform the data and wrap it back into DataFrames
+        X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
+        X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
+        data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
+        data.to_parquet("../../data/filter/SelectKBestChi2_" + str(dataset_id) + ".parquet")
 
-    # SelectKBest
-    print("Filter Method: SelectKBest, Dataset: " + str(dataset_id))
-    selectKBest = SelectKBest(f_classif, k=2)
-    selectKBest.fit(X_train, y_train)
-    X_train_new = selectKBest.transform(X_train)
-    X_test_new = selectKBest.transform(X_test)
-    selected_features = X_train.columns[selectKBest.get_support()]
-    # Transform the data and wrap it back into DataFrames
-    X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
-    X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
-    data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
-    data.to_parquet("../../data/filter/SelectKBest_" + str(dataset_id) + ".parquet")
-
+    # SelectPercentile - Mutual_info_classif
+    print("Filter Method: SelectPercentile, Score Function: Mutual Info Classifier, Dataset: " + str(dataset_id))
+    try:
+        data = pd.read_parquet("../../data/filter/SelectPercentileMutualInfo_" + str(dataset_id) + ".parquet")
+        print("File exists, next method" + str(data.head()) + "\n\n")
+    except FileNotFoundError:
+        print("Calculate Feature Selection \n\n")
+        selectKBest = SelectPercentile(score_func=mutual_info_classif, percentile=50)
+        selectKBest.fit(X_train, y_train)
+        X_train_new = selectKBest.transform(X_train)
+        X_test_new = selectKBest.transform(X_test)
+        selected_features = X_train.columns[selectKBest.get_support()]
+        # Transform the data and wrap it back into DataFrames
+        X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
+        X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
+        data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
+        data.to_parquet("../../data/filter/SelectPercentileMutualInfo_" + str(dataset_id) + ".parquet")
 
     # Select From Model (Linear SVC)
-    print("Filter Method: Linear SVC, Dataset: " + str(dataset_id))
-    lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(X_train, y_train)
-    model = SelectFromModel(lsvc, prefit=True)
-    X_train_new = model.transform(X_train)
-    X_test_new = model.transform(X_test)
-    selected_features = X_train.columns[model.get_support()]
-    X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
-    X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
-    data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
-    data.to_parquet("../../data/wrapper/SklearnLinearSVC_" + str(dataset_id) + ".parquet")
-
+    print("Filter Method: Linear SVC, Penalty: L1, Dataset: " + str(dataset_id))
+    try:
+        data = pd.read_parquet("../../data/wrapper/SklearnLinearSVC_" + str(dataset_id) + ".parquet")
+        print("File exists, next method" + str(data.head()) + "\n\n")
+    except FileNotFoundError:
+        print("Calculate Feature Selection \n\n")
+        lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(X_train, y_train)
+        model = SelectFromModel(lsvc, prefit=True)
+        X_train_new = model.transform(X_train)
+        X_test_new = model.transform(X_test)
+        selected_features = X_train.columns[model.get_support()]
+        X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
+        X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
+        data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
+        data.to_parquet("../../data/wrapper/SklearnLinearSVC_" + str(dataset_id) + ".parquet")
 
     # Select From Model (Extra Trees Classifier)
     print("Filter Method: ExtraTreeClassifier, Dataset: " + str(dataset_id))
-    clf = ExtraTreesClassifier(n_estimators=50)
-    clf = clf.fit(X_train, y_train)
-    model = SelectFromModel(clf, prefit=True)
-    X_train_new = model.transform(X_train)
-    X_test_new = model.transform(X_test)
-    selected_features = X_train.columns[model.get_support()]
-    X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
-    X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
-    data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
-    data.to_parquet("../../data/wrapper/SklearnExtraTreeClassifier_" + str(dataset_id) + ".parquet")
+    try:
+        data = pd.read_parquet("../../data/wrapper/SklearnExtraTreeClassifier_" + str(dataset_id) + ".parquet")
+        print("File exists, next method" + str(data.head()) + "\n\n")
+    except FileNotFoundError:
+        print("Calculate Feature Selection \n\n")
+        clf = ExtraTreesClassifier(n_estimators=50)
+        clf = clf.fit(X_train, y_train)
+        model = SelectFromModel(clf, prefit=True)
+        X_train_new = model.transform(X_train)
+        X_test_new = model.transform(X_test)
+        selected_features = X_train.columns[model.get_support()]
+        X_train_new = pd.DataFrame(X_train_new, columns=selected_features, index=X_train.index)
+        X_test_new = pd.DataFrame(X_test_new, columns=selected_features, index=X_test.index)
+        data = concat_data(X_train_new, y_train, X_test_new, y_test, "target")
+        data.to_parquet("../../data/wrapper/SklearnExtraTreeClassifier_" + str(dataset_id) + ".parquet")
 
 
 def run_process_method(dataset_id):
