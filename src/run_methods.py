b9fed5074d05a5dd516f64b86de252760790b1dd
bastis007 <schaefer.bastian@mail.de>
2025-10-22 14:37:52 +0200

    Add notifications and remove some not working methods-dataset pairs

--- src/run_methods.py
+++ src/run_methods.py
@@ -4,12 +4,19 @@ import time
 
 import pandas as pd
 
+import subprocess
+
+
+def send_notification(title, message):
+    script = f'display notification "{message}" with title "{title}"'
+    subprocess.run(['osascript', '-e', script])
+
 def run_methods(dataset_ids):
     methods = glob.glob("feature_selection_method/*/*/*.py")
     with open("results/times.txt", 'a') as f:
         f.write(f" *************************************************** \n Time per Method and Dataset \n *************************************************** \n\n")
     for method in methods:
-        if any(skip in method for skip in ["Add_Pandas_Metafeatures.py", "MUFS.py", "Metrics.py", "MAFESE.py"]):
+        if any(skip in method for skip in ["Add_Pandas_Metafeatures.py", "MUFS.py", "Metrics.py", "MAFESE.py", "BioAutoML.py"]):
             continue
         method_name = method.split("/")[-1].split(".")[0]
         path_name = method.split('method/')[-1].replace(str("/" + method_name + ".py"), '')
@@ -17,19 +24,33 @@ def run_methods(dataset_ids):
         module_path = f"src.feature_selection_method.{path_name.replace('/', '.')}.{method_name}"
 
         for dataset_id in dataset_ids:
-            output_path = f"data/{path_name}_{dataset_id}.parquet"
-            try:
-                pd.read_parquet(output_path)
-                print(f"✅ File exists for {method_name}, dataset {dataset_id}")
-            except FileNotFoundError:
-                print(f"⚠️ File missing for dataset {dataset_id} — generating with {method_name}.main()")
-                module = importlib.import_module(module_path)
-                start = time.time()
-                module.main(dataset_id)
-                end = time.time()
-                method_time = end - start
-                with open("results/times.txt", 'a') as f:
-                    f.write(f"{method_name} - {dataset_id}: {method_time}\n")
+            if method_name == "SklearnBackwardSFS" and dataset_id == 167210:
+                continue
+            elif method_name == "SklearnBackwardSFS" and dataset_id == 168757:
+                continue
+            elif method_name == "SklearnBackwardSFS" and dataset_id == 189354:
+                continue
+            elif method_name == "SklearnExtraTreesClassifier" and dataset_id == 167210:
+                continue
+            elif method_name == "SklearnExtraTreesClassifier" and dataset_id == 168757:
+                continue
+            elif method_name == "SklearnExtraTreesClassifier" and dataset_id == 189354:
+                continue
+            else:
+                output_path = f"data/{path_name}_{dataset_id}.parquet"
+                try:
+                    pd.read_parquet(output_path)
+                    print(f"✅ File exists for {method_name}, dataset {dataset_id}")
+                except FileNotFoundError:
+                    print(f"⚠️ File missing for dataset {dataset_id} — generating with {method_name}.main()")
+                    module = importlib.import_module(module_path)
+                    start = time.time()
+                    module.main(dataset_id)
+                    end = time.time()
+                    method_time = end - start
+                    with open("results/times.txt", 'a') as f:
+                        f.write(f"{method_name} - {dataset_id}: {method_time}\n")
+                        send_notification("File saved", f"{method_name} - {dataset_id}: {method_time}")
 
 
 if __name__ == "__main__":
